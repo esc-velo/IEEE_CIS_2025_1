413e0dcf5f99088fe44dfd34b7aacb7099fc9854
Rahini Raneru <rahiniraneru@yahoo.com>
2025-06-30 01:19:15 +0530

    P&P team

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -3,11 +3,10 @@ import type { TeamMember } from '../components/Team';
 export const ccdata: TeamMember[] = [
   {
     id: 1,
-    name: 'Aarav Mehta',
-    role: 'Chairperson',
-    email: 'aarav@example.com',
-    bio: 'Leads the club and ensures smooth execution of all plans.',
-    image: '/images/cc/aarav.png',
+    name: 'Parag Goel',
+    role: 'Programs and Promotions - Head',
+    email: '',
+    image: '/images/cc/parag.png',
     socialMedia: {
       instagram: 'https://instagram.com/aarav',
       linkedin: 'https://linkedin.com/in/aaravmehta',
@@ -15,26 +14,43 @@ export const ccdata: TeamMember[] = [
   },
   {
     id: 2,
-    name: 'Priya Sharma',
-    role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
-    image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
+    name: 'Shubhanshi Shekhar',
+    role: 'Programs and Promotions - Joint Head',
+    email: 'shubhanshi.2427010175@muj.manipal.edu',
+    image: 'https://images.prismic.io/ieeemuj/aGGYWnfc4bHWi0KE_Shubhanshi_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
-      twitter: 'https://twitter.com/priyasharma',
-      github: 'https://github.com/priyadev',
+      linkedin: 'https://www.linkedin.com/in/shubhanshi-shekhar-81a649348',
     },
   },
   {
     id: 3,
-    name: 'Priya Sharma',
-    role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
+    name: 'Savni Goyal',
+    role: 'Senior Coordinator of Programs and Promotions ',
+    email: 'savni.2427030537@muj.manipal.edu',
+    image: 'https://images.prismic.io/ieeemuj/aFrTeXfc4bHWirWt_Savni_CIS_CC_25-26.jpeg?auto=format,compress',
+    socialMedia: {
+      linkedin: 'https://www.linkedin.com/in/savnigoyal?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+      github: 'https://github.com/savnigoyal',
+    },
+  },
+  {
+    id: 4,
+    name: 'Yashvi Jangir',
+    role: 'Senior Coordinator of Programs and Promotions ',
+    email: 'yashvi.2430030200@muj.manipal.edu',
     image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
-      twitter: 'https://twitter.com/priyasharma',
-      github: 'https://github.com/priyadev',
+      linkedin: 'https://www.linkedin.com/in/yashvi-jangir-43b23b212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    },
+  },
+  {
+    id: 5,
+    name: 'Manya Gupta',
+    role: 'Senior Coordinator of Programs and Promotions ',
+    email: 'manya.2427010351@muj.manipal.edu',
+    image: '',
+    socialMedia: {
+
     },
   },
 ];
