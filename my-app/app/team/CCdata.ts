73b26d2f4ba715fab081342c13a481d12495c87a
GAURI ARORA <gauriarora@GAURIs-MacBook-Air-2.local>
2025-07-01 00:25:57 +0530

    Added LinkedIn and GitHub links to socialMedia fields

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -38,68 +38,88 @@ export const ccdata: TeamMember[] = [
     },
   },
   {
-  id: 3,
+  id: 4,
   name: 'Drishti Nandwani',
   role: 'Technical Team',
   bio: 'Technical - Webmaster',
   image: 'https://images.prismic.io/ieeemuj/aFrTXnfc4bHWirWG_Drishti_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/drishtinandwani11/',
+  },
 },
 {
-  id: 4,
+  id: 5,
   name: 'Gauri Arora',
   role: 'Technical Team',
   bio: 'Technical - Webmaster',
   image: 'https://images.prismic.io/ieeemuj/aFrTZHfc4bHWirWM_Gauri_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/gauri-arora-566913321?',
+    github: 'https://github.com/gauriarora-cyber',
+  },
 },
 {
-  id: 5,
+  id: 6,
   name: 'Rahini Raneru',
   role: 'Technical Team',
   bio: 'Technical - Webmaster',
   image: 'https://images.prismic.io/ieeemuj/aFrTdHfc4bHWirWr_Rahini_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/rahini-raneru-6493552b6',
+    github: 'https://github.com/esc-velo'
+  },
 },
 {
-  id: 6,
+  id: 7,
   name: 'Vaisakhi Das',
   role: 'Technical Team',
   bio: 'Technical - Webmaster',
   image: 'https://images.prismic.io/ieeemuj/aFrTfHfc4bHWirWw_Vaisakhi_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/vaisakhi-das-b03858303/',
+    github:'https://github.com/VaisakhiDas'
+  },
 },
 {
-  id: 7,
+  id: 8,
   name: 'Garv Sharma',
   role: 'SM & GD',
   bio: 'SM & GD - Head',
   image: 'https://images.prismic.io/ieeemuj/aFrTYHfc4bHWirWI_Garv_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/garv-sharma-17991536b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+  },
 },
 {
-  id: 8,
+  id: 9,
   name: 'S Hriday',
   role: 'SM & GD',
   bio: 'SM & GD - Joint Head',
   image: 'https://images.prismic.io/ieeemuj/aFqB8Xfc4bHWiqQO_Hriday_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/hriday-020458343?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    github: 'https://github.com/DCypherHriday?fbclid=PAQ0xDSwLGMGVleHRuA2FlbQIxMQABp9J-BrzJkX021cBzm1o7Ptwz8FLrHxR1z-mv_0vNdBaXs_Zxv0wcXMhPh52X_aem_UD0PtuVbMt8k5KlG_51wkg'
+  },
 },
 {
-  id: 9,
+  id: 10,
   name: 'PRATEEK RAJENDRA',
   role: 'SM & GD',
   bio: 'SM & GD - Senior Coordinator',
   image: 'https://images.prismic.io/ieeemuj/aFrTb3fc4bHWirWg_Prateek_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'http://www.linkedin.com/in/prateek-borse-412b2832a',
+  },
 },
 {
-  id: 10,
+  id: 11,
   name: 'Abhinav Kumar Tripathi',
   role: 'SM & GD',
   bio: 'SM & GD - Senior Coordinator',
   image: 'https://images.prismic.io/ieeemuj/aFrTW3fc4bHWirWC_Abhinav_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/abhinav-kumar-tripathi-ba4638360?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+  },
 },
 
 ]
