0db9cf84a7cc7ebfd181a5f0839d4478f46992ca
Drishti Nandwani <drishti11.nandwani05@gmail.com>
2025-06-30 15:15:50 +0530

    Added Sponsorship & Curations

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -3,26 +3,57 @@ import type { TeamMember } from '../components/Team';
 export const ccdata: TeamMember[] = [
   {
     id: 1,
-    name: 'Aarav Mehta',
-    role: 'Chairperson',
-    email: 'aarav@example.com',
-    bio: 'Leads the club and ensures smooth execution of all plans.',
-    image: '/images/cc/aarav.png',
+    name: 'Aaryan Rathee',
+    role: 'Sponsorship & Curations - Head',
+    image: 'https://images.prismic.io/ieeemuj/aFrTWXfc4bHWirV__Aaryan_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
-      instagram: 'https://instagram.com/aarav',
-      linkedin: 'https://linkedin.com/in/aaravmehta',
+      linkedin: 'https://www.linkedin.com/in/aaryanrathee',
+      instagram: 'https://www.instagram.com/ft.aaryanrathee',
+      
     },
   },
+
   {
     id: 2,
-    name: 'Priya Sharma',
-    role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
-    image: '/images/cc/priya.png',
+    name: 'Ekaagra Gupta',
+    role: 'Sponsorship & Curations - Joint Head',
+    image: 'https://images.prismic.io/ieeemuj/aFrTX3fc4bHWirWH_Ekaagra_CIS_CC_25-26.png?auto=format,compress',
     socialMedia: {
-      twitter: 'https://twitter.com/priyasharma',
-      github: 'https://github.com/priyadev',
+      linkedin: 'http://www.linkedin.com/in/ekaagragupta',
+      github: 'https://github.com/ekaagragupta',
     },
   },
+
+ {
+    id: 3,
+    name: 'Veer Chavan',
+    role: 'Sponsorship & Curations - Senior Coordinator',
+    image: 'https://images.prismic.io/ieeemuj/aFrTf3fc4bHWirWx_Veer_CIS_CC_25-26.jpg?auto=format,compress',
+    socialMedia: {
+      linkedin: 'https://www.linkedin.com/in/veer-chavan-84b35932a/',
+      instagram: 'https://www.instagram.com/veer.chavan_/',
+    },
+  },
+
+
+   {
+    id: 4,
+    name: 'Arnav jain',
+    role: 'Sponsorship & Curations - Senior Coordinator',
+    image: '',
+    socialMedia: {
+      linkedin: 'https://www.linkedin.com/in/arnavvjainnn',
+    },
+  },
+
+  {
+    id: 5,
+    name: 'Sanket',
+    role: 'Sponsorship & Curations - Senior Coordinator',
+    image: 'https://images.prismic.io/ieeemuj/aFrTZXfc4bHWirWP_HarshGandash_CIS_CC_25-26.jpg?auto=format,compress',
+    socialMedia: {
+      linkedin: '',
+    },
+  },
+
 ];
