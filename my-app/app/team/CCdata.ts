3af66b44e3441990a0a9babd9949d1dd911cbd23
ekaagra gupta <ekaagrag2006@gmail.com>
2025-07-01 13:21:16 +0530

    Added finance team data

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -3,26 +3,36 @@ import type { TeamMember } from '../components/Team';
 export const ccdata: TeamMember[] = [
   {
     id: 1,
-    name: 'Aarav Mehta',
-    role: 'Chairperson',
-    email: 'aarav@example.com',
-    bio: 'Leads the club and ensures smooth execution of all plans.',
-    image: '/images/cc/aarav.png',
+    name: 'Mahi Malhotra ',
+    role: ' Finance - Head',
+    image: 'https://images.prismic.io/ieeemuj/aFrTaXfc4bHWirWW_Mahi_CIS_CC_25-26.HEIC?auto=format,compress',
     socialMedia: {
-      instagram: 'https://instagram.com/aarav',
-      linkedin: 'https://linkedin.com/in/aaravmehta',
+      linkedin: 'https://www.linkedin.com/in/mahi-malhotra-674630367',
     },
   },
   {
-    id: 2,
-    name: 'Priya Sharma',
-    role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
-    image: '/images/cc/priya.png',
+   id: 2,
+    name: 'Nayandeep Dey ',
+    role: 'Finance- Joint head of Finance',
+    image: 'https://images.prismic.io/ieeemuj/aFrTbHfc4bHWirWb_Nayandeep_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
-      twitter: 'https://twitter.com/priyasharma',
-      github: 'https://github.com/priyadev',
+      linkedin: 'https://www.linkedin.com/in/nayandeep-dey-17a60721a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     },
   },
-];
+    {
+   id: 3,
+    name: 'Kritika Mittal ',
+    role: 'Finance - Senior coordinator',
+    image: 'https://images.prismic.io/ieeemuj/aFrTZ3fc4bHWirWT_Kritika_CIS_CC_25-26.jpg?auto=format,compress',
+    },
+    {
+   id: 3,
+    name: 'Manya Moonat ',
+    role: 'Finance - Senior coordinator',
+    image: 'https://images.prismic.io/ieeemuj/aFrTanfc4bHWirWY_Manya_CIS_CC_25-26.jpeg?auto=format,compress',
+    socialMedia: {
+      linkedin: 'https://www.linkedin.com/in/manya-moonat-15a090270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    },
+    },
+
+]
