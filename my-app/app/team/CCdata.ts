99db01ebaabd4e86ea711f8e2715c77d56c53c02
Rahini Raneru <rahiniraneru@yahoo.com>
2025-07-01 17:09:50 +0530

    Final_P&Pdeets

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -5,7 +5,6 @@ export const ccdata: TeamMember[] = [
     id: 1,
     name: 'Parag Goel',
     role: 'Programs and Promotions - Head',
-    email: 'parag.2430040103@muj.manipal.edu',
     image: 'https://images.prismic.io/ieeemuj/aGPAKXfc4bHWi6Ui_Parag_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
       instagram: 'https://www.instagram.com/parag._10?igsh=aWw5YXFiOTFoaHAw',
@@ -16,7 +15,6 @@ export const ccdata: TeamMember[] = [
     id: 2,
     name: 'Shubhanshi Shekhar',
     role: 'Programs and Promotions - Joint Head',
-    email: 'shubhanshi.2427010175@muj.manipal.edu',
     image: 'https://images.prismic.io/ieeemuj/aGGYWnfc4bHWi0KE_Shubhanshi_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/shubhanshi-shekhar-81a649348',
@@ -26,7 +24,6 @@ export const ccdata: TeamMember[] = [
     id: 3,
     name: 'Savni Goyal',
     role: 'Senior Coordinator of Programs and Promotions ',
-    email: 'savni.2427030537@muj.manipal.edu',
     image: 'https://images.prismic.io/ieeemuj/aFrTeXfc4bHWirWt_Savni_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/savnigoyal?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
@@ -37,7 +34,6 @@ export const ccdata: TeamMember[] = [
     id: 4,
     name: 'Yashvi Jangir',
     role: 'Senior Coordinator of Programs and Promotions ',
-    email: 'yashvi.2430030200@muj.manipal.edu',
     image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/yashvi-jangir-43b23b212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
@@ -47,7 +43,6 @@ export const ccdata: TeamMember[] = [
     id: 5,
     name: 'Manya Gupta',
     role: 'Senior Coordinator of Programs and Promotions ',
-    email: 'manya.2427010351@muj.manipal.edu',
     image: '',
     socialMedia: {
       
