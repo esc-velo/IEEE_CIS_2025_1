cd6594f73227e8b9d99ead26153b4ecfbb7fb5fd
Kanishka Bhatia <bhatiakanishka930@gmail.com>
2025-07-01 19:06:09 +0530

    refactor : removed unecessary data

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -5,8 +5,7 @@ export const ccdata: TeamMember[] = [
     id: 1,
     name: 'A',
     role: 'Chairperson',
-    email: 'aarav@example.com',
-    bio: 'Leads the club and ensures smooth execution of all plans.',
+   
     image: '/images/cc/aarav.png',
     socialMedia: {
       instagram: 'https://instagram.com/aarav',
@@ -17,8 +16,7 @@ export const ccdata: TeamMember[] = [
     id: 2,
     name: 'Priya Sharma',
     role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
+    
     image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
       twitter: 'https://twitter.com/priyasharma',
@@ -29,8 +27,7 @@ export const ccdata: TeamMember[] = [
     id: 3,
     name: 'Priya Sharma',
     role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
+   
     image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
       twitter: 'https://twitter.com/priyasharma',
@@ -41,7 +38,7 @@ export const ccdata: TeamMember[] = [
   id: 3,
   name: 'Drishti Nandwani',
   role: 'Technical - Webmaster',
-  bio: 'Handles technical aspects and maintains the IEEE CIS MUJ website.',
+ 
   image: 'https://images.prismic.io/ieeemuj/aFrTXnfc4bHWirWG_Drishti_CIS_CC_25-26.jpg?auto=format,compress',
   socialMedia: {},
 },
@@ -49,7 +46,7 @@ export const ccdata: TeamMember[] = [
   id: 4,
   name: 'Gauri Arora',
   role: 'Technical - Webmaster',
-  bio: 'Contributes to core website development and automation workflows.',
+  
   image: 'https://images.prismic.io/ieeemuj/aFrTZHfc4bHWirWM_Gauri_CIS_CC_25-26.jpg?auto=format,compress',
   socialMedia: {},
 },
@@ -57,7 +54,7 @@ export const ccdata: TeamMember[] = [
   id: 5,
   name: 'Rahini Raneru',
   role: 'Technical - Webmaster',
-  bio: 'Supports development and technical implementation for web projects.',
+ 
   image: 'https://images.prismic.io/ieeemuj/aFrTdHfc4bHWirWr_Rahini_CIS_CC_25-26.jpg?auto=format,compress',
   socialMedia: {},
 },
@@ -65,7 +62,7 @@ export const ccdata: TeamMember[] = [
   id: 6,
   name: 'Vaisakhi Das',
   role: 'Technical - Webmaster',
-  bio: 'Works on the frontend/backend and maintains event-based web tools.',
+  
   image: 'https://images.prismic.io/ieeemuj/aFrTfHfc4bHWirWw_Vaisakhi_CIS_CC_25-26.jpg?auto=format,compress',
   socialMedia: {},
 },
