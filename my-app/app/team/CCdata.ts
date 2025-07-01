ae3323723bdfcd9cc99d4dab31e51c9e4b61aed6
Rahini Raneru <rahiniraneru@yahoo.com>
2025-07-01 19:15:11 +0530

    FinalP&Pdeets

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -4,7 +4,7 @@ export const ccdata: TeamMember[] = [
   {
     id: 1,
     name: 'Parag Goel',
-    role: 'Programs and Promotions - Head',
+    role: 'Head of Programs & Promotions',
     image: 'https://images.prismic.io/ieeemuj/aGPAKXfc4bHWi6Ui_Parag_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
       instagram: 'https://www.instagram.com/parag._10?igsh=aWw5YXFiOTFoaHAw',
@@ -14,7 +14,7 @@ export const ccdata: TeamMember[] = [
   {
     id: 2,
     name: 'Shubhanshi Shekhar',
-    role: 'Programs and Promotions - Joint Head',
+    role: 'Joint Head of Programs & Promotions',
     image: 'https://images.prismic.io/ieeemuj/aGGYWnfc4bHWi0KE_Shubhanshi_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/shubhanshi-shekhar-81a649348',
@@ -23,7 +23,7 @@ export const ccdata: TeamMember[] = [
   {
     id: 3,
     name: 'Savni Goyal',
-    role: 'Senior Coordinator of Programs and Promotions ',
+    role: 'Senior Coordinator of Programs & Promotions',
     image: 'https://images.prismic.io/ieeemuj/aFrTeXfc4bHWirWt_Savni_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/savnigoyal?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
@@ -33,7 +33,7 @@ export const ccdata: TeamMember[] = [
   {
     id: 4,
     name: 'Yashvi Jangir',
-    role: 'Senior Coordinator of Programs and Promotions ',
+    role: 'Senior Coordinator of Programs & Promotions',
     image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/yashvi-jangir-43b23b212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
@@ -42,44 +42,12 @@ export const ccdata: TeamMember[] = [
   {
     id: 5,
     name: 'Manya Gupta',
-    role: 'Senior Coordinator of Programs and Promotions ',
+    role: 'Senior Coordinator of Programs & Promotions',
     image: '',
     socialMedia: {
       
     },
   },
-  {
-  id: 19,
-  name: 'Drishti Nandwani',
-  role: 'Technical - Webmaster',
-  bio: 'Handles technical aspects and maintains the IEEE CIS MUJ website.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTXnfc4bHWirWG_Drishti_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
-{
-  id: 22,
-  name: 'Gauri Arora',
-  role: 'Technical - Webmaster',
-  bio: 'Contributes to core website development and automation workflows.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTZHfc4bHWirWM_Gauri_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
-{
-  id: 20,
-  name: 'Rahini Raneru',
-  role: 'Technical - Webmaster',
-  bio: 'Supports development and technical implementation for web projects.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTdHfc4bHWirWr_Rahini_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
-{
-  id: 21,
-  name: 'Vaisakhi Das',
-  role: 'Technical - Webmaster',
-  bio: 'Works on the frontend/backend and maintains event-based web tools.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTfHfc4bHWirWw_Vaisakhi_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
 ]
   
   
