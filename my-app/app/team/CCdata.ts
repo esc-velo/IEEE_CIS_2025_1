e21f90f501be57e2a3e4d6d7e82e353f395b173f
Rahini Raneru <rahiniraneru@yahoo.com>
2025-07-01 19:17:13 +0530

    finalP&Pdeets

--- my-app/app/team/CCdata.ts
+++ my-app/app/team/CCdata.ts
@@ -3,11 +3,9 @@ import type { TeamMember } from '../components/Team';
 export const ccdata: TeamMember[] = [
   {
     id: 1,
-    name: 'A',
-    role: 'Chairperson',
-    email: 'aarav@example.com',
-    bio: 'Leads the club and ensures smooth execution of all plans.',
-    image: '/images/cc/aarav.png',
+    name: 'Parag Goel',
+    role: 'Head of Programs & Promotions',
+    image: 'https://images.prismic.io/ieeemuj/aGPAKXfc4bHWi6Ui_Parag_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
       instagram: 'https://www.instagram.com/parag._10?igsh=aWw5YXFiOTFoaHAw',
       linkedin: 'https://www.linkedin.com/in/parag-goel-3865a6262?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
@@ -15,59 +13,41 @@ export const ccdata: TeamMember[] = [
   },
   {
     id: 2,
-    name: 'Priya Sharma',
-    role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
-    image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
+    name: 'Shubhanshi Shekhar',
+    role: 'Joint Head of Programs & Promotions',
+    image: 'https://images.prismic.io/ieeemuj/aGGYWnfc4bHWi0KE_Shubhanshi_CIS_CC_25-26.jpg?auto=format,compress',
     socialMedia: {
       linkedin: 'https://www.linkedin.com/in/shubhanshi-shekhar-81a649348',
     },
   },
   {
     id: 3,
-    name: 'Priya Sharma',
-    role: 'Vice Chairperson',
-    email: 'priya@example.com',
-    bio: 'Supports leadership and bridges communication among teams.',
-    image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
+    name: 'Savni Goyal',
+    role: 'Senior Coordinator of Programs & Promotions',
+    image: 'https://images.prismic.io/ieeemuj/aFrTeXfc4bHWirWt_Savni_CIS_CC_25-26.jpeg?auto=format,compress',
     socialMedia: {
-      twitter: 'https://twitter.com/priyasharma',
-      github: 'https://github.com/priyadev',
+      linkedin: 'https://www.linkedin.com/in/savnigoyal?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+      github: 'https://github.com/savnigoyal',
     },
   },
   {
-  id: 3,
-  name: 'Drishti Nandwani',
-  role: 'Technical - Webmaster',
-  bio: 'Handles technical aspects and maintains the IEEE CIS MUJ website.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTXnfc4bHWirWG_Drishti_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
-{
-  id: 4,
-  name: 'Gauri Arora',
-  role: 'Technical - Webmaster',
-  bio: 'Contributes to core website development and automation workflows.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTZHfc4bHWirWM_Gauri_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
-{
-  id: 5,
-  name: 'Rahini Raneru',
-  role: 'Technical - Webmaster',
-  bio: 'Supports development and technical implementation for web projects.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTdHfc4bHWirWr_Rahini_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
-{
-  id: 6,
-  name: 'Vaisakhi Das',
-  role: 'Technical - Webmaster',
-  bio: 'Works on the frontend/backend and maintains event-based web tools.',
-  image: 'https://images.prismic.io/ieeemuj/aFrTfHfc4bHWirWw_Vaisakhi_CIS_CC_25-26.jpg?auto=format,compress',
-  socialMedia: {},
-},
+    id: 4,
+    name: 'Yashvi Jangir',
+    role: 'Senior Coordinator of Programs & Promotions',
+    image: 'https://images.prismic.io/ieeemuj/aFrTgHfc4bHWirWy_Yashvi_CIS_CC_25-26.jpeg?auto=format,compress',
+    socialMedia: {
+      linkedin: 'https://www.linkedin.com/in/yashvi-jangir-43b23b212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    },
+  },
+  {
+    id: 5,
+    name: 'Manya Gupta',
+    role: 'Senior Coordinator of Programs & Promotions',
+    image: '',
+    socialMedia: {
+      
+    },
+  },
 ]
   
   
