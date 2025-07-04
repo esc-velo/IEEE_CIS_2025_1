b0bed399b0d6327c8f311ceefc55a5fe730dae57
Kanishka Bhatia <bhatiakanishka930@gmail.com>
2025-07-04 09:03:14 +0530

    fix:build error

--- app/components/FAQs.tsx
+++ app/components/FAQs.tsx
@@ -2,7 +2,7 @@
  
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { ChevronDown, Mail } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import { cn } from '../lib/utils';
 
  
