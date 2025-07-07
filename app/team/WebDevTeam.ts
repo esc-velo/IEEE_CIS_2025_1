151206000ac8febd262f42a76094f886739cf780
Ekaagra Gupta <ekaagrag2006@gmail.com>
2025-07-07 11:34:06 +0530

    WebDevTeam.ts
    added technical team data

--- app/team/WebDevTeam.ts
+++ app/team/WebDevTeam.ts
@@ -17,4 +17,56 @@ export const webdevTeamData: TeamMember[] = [
       
     },
   },
+  {
+  id: 2,
+  name: 'Drishti Nandwani',
+  role: 'Webmaster',
+  image: 'https://images.prismic.io/ieeemuj/aFrTXnfc4bHWirWG_Drishti_CIS_CC_25-26.jpg?auto=format,compress',
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/drishtinandwani11/',
+   },
+  },
+ {
+   id: 3,
+  name: 'Ekaagra gupta',
+  role: 'Webmaster',
+ 
+  image: 'https://images.prismic.io/ieeemuj/aGth4EMqNJQqHlkc_Ekaagra_cis_CC_25-26.jpeg?auto=format,compress',
+  socialMedia: {
+    linkedin: 'www.linkedin.com/in/ekaagragupta',
+    github: 'https://github.com/ekaagragupta'
+  },
+ },
+{
+  id: 4,
+  name: 'Gauri Arora',
+  role: 'Webmaster',
+  image: 'https://images.prismic.io/ieeemuj/aFrTZHfc4bHWirWM_Gauri_CIS_CC_25-26.jpg?auto=format,compress',
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/gauri-arora-566913321?',
+    github: 'https://github.com/gauriarora-cyber',
+},
+  },
+
+{
+  id: 5,
+  name: 'Rahini Raneru',
+  role: 'Webmaster',
+ 
+  image: 'https://images.prismic.io/ieeemuj/aFrTdHfc4bHWirWr_Rahini_CIS_CC_25-26.jpg?auto=format,compress',
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/rahini-raneru-6493552b6',
+    github: 'https://github.com/esc-velo'
+  },
+},
+{
+  id: 6,
+  name: 'Vaisakhi Das',
+  role: 'Webmaster', 
+  image: 'https://images.prismic.io/ieeemuj/aFrTfHfc4bHWirWw_Vaisakhi_CIS_CC_25-26.jpg?auto=format,compress',
+  socialMedia: {
+    linkedin: 'https://www.linkedin.com/in/vaisakhi-das-b03858303/',
+    github:'https://github.com/VaisakhiDas'
+  },
+},
 ]
